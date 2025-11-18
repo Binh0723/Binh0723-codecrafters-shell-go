3c714117d3807b98aa832ac8c122eabb2b4c7778
Binh0723 <binhtrancongduc2307@gmail.com>
2025-11-17 20:56:29 -0500

    [any message]

--- app/main.go
+++ app/main.go
@@ -13,6 +13,12 @@ var _ = os.Stdout
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
+
+	builtins := map[string]bool {
+		"echo": true,
+		"type": true,
+		"exit": true,
+	}
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		reader := bufio.NewReader(os.Stdin)
@@ -29,7 +35,13 @@ func main() {
 		} else if parts[0] == "echo" {
 			new_line := strings.Join(parts[1:], " ")
 			fmt.Fprintf(os.Stdout, "%s\n", new_line)
-		} else {
+		} else if parts[0] == "type"{
+			if builtins[parts[1]] {
+				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", parts[1])
+			} else {
+				fmt.Fprintf(os.Stdout, "%s: not found\n", parts[1])
+			}
+		}else {
 			fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 		}	
 	}
